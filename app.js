e21181682f35451302d7a7e656158bec66c163a1
ktrip96 <ktrip96@gmail.com>
2023-10-16 00:31:25 +0300

    Redirections of normal users

--- app.js
+++ app.js
@@ -1,61 +1,60 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const { MongoClient, ObjectId } = require('mongodb')
+require('dotenv').config()
 
-app.get("/", (req, res) => res.type('html').send(html));
+const app = express()
+const port = 3001
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Create a MongoDB connection pool
+const client = new MongoClient(process.env.MONGOURL)
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+// Function to connect to the MongoDB database using the connection pool
+async function connectToDatabase() {
+	try {
+		await client.connect()
+	} catch (err) {
+		console.error('Error connecting to MongoDB:', err)
+	}
+}
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+// Middleware to connect to the database before handling requests
+app.use(async (req, res, next) => {
+	await connectToDatabase()
+
+	req.dbClient = client
+	req.db = client.db('nfc-database')
+	next()
+})
+
+// Your route to handle GET requests
+app.get('/redirect', async (req, res) => {
+	const id = new ObjectId(req.query.id)
+
+	console.log('Id is :, ', id)
+
+	const collection = req.db.collection('clients')
+
+	console.log('Collection is : ', collection)
+
+	// Perform your MongoDB query to check if the ID matches a user
+	const user = await collection.findOne({ _id: id })
+
+	console.log('user is : ', user)
+
+	if (user) {
+		// Redirect to another domain
+		res.redirect(user.url) // Replace with the desired domain
+	} else {
+		// Send a "Something went wrong" message
+		res.send('<h1>Something went wrong</h1>')
+	}
+})
+
+app.get('/', async (req, res) => {
+	res.send('Hello!!!')
+})
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
